84d17fca110a17736aa435d7ea24cd0c8e362cd0
msaharia <manab@ucar.edu>
2018-03-09 16:41:10 -0700

    ASMO files for SUMMA

---
+++ summa/summa.py
@@ -0,0 +1,92 @@
+import os
+import math
+import string
+import numpy as np
+import sys
+sys.path.append('../src')
+import util
+
+#######################################################
+# USER SPECIFIC SECTION
+#======================================================
+modelpath = "/glade/p/work/manab/fcast/newsumma/summa"
+controlFileName = "summa_fileManager_new.txt"
+appInputFiles = [ os.path.join(modelpath, 'PNW/summa_zLocalParamInfo.txt')]
+appInputTmplts = ["summa_zLocalParamInfo_template.txt"]
+
+#######################################################
+# FUNCTION: GENERATE MODEL INPUT FILE
+#======================================================
+def genAppInputFile(inputData, appTmpltFiles, appInputFiles, nInputs, inputNames):
+    nfiles = len(appInputFiles)
+    for i in range(nfiles):
+        infile = open(appTmpltFiles[i], "r")
+        outfile = open(appInputFiles[i], "w")
+        while 1:
+            lineIn  = infile.readline()
+            if lineIn == "":
+                break
+            lineLen = len(lineIn)
+            newLine = lineIn
+            if nInputs > 0:
+                for fInd in range(nInputs):
+                    strLen = len(inputNames[fInd])
+                    while 1:
+                        sInd = newLine.find(inputNames[fInd])
+                        if sInd < 0:
+                            break
+                        sdata = '%7.3f' % inputData[fInd]
+                        strdata = str(sdata)
+                        ntidx = sInd + strLen
+                        lineTemp = newLine[0:sInd] + strdata +  " " + newLine[ntidx:lineLen+1]
+                        newLine = lineTemp
+                        lineLen = len(newLine)
+            outfile.write(newLine)
+        infile.close()
+        outfile.close()
+    return
+
+def allindices(str1, str2, listindex = [], offset=0):
+    """
+    return all indeces of str2 found in str1
+    """
+    i = str1.find(str2, offset)
+    while i >= 0:
+        listindex.append(i)
+        i = str1.find(str2, i + len(str2))
+    return listindex
+
+#######################################################
+# FUNCTION: RUN MODEL
+#====================================================== 
+def runApplication():
+    cwd = os.getcwd()
+    os.chdir(modelpath) 
+    os.system("csh run_HHDW1_summa.csh")
+    os.chdir(cwd)
+    return
+
+#######################################################
+# FUNCTION: CALCULATE DESIRE OUTPUT
+#======================================================
+def getOutput():
+    ## compute RMSE from 1970-01-01 to 2007-12-31
+    #Qsim = np.loadtxt(modelpath + "sample_model/output/wbvars.txt.HHWM8", skiprows=1)[0:13878,18]
+    #Qobs = np.loadtxt(modelpath + "sample_model/input/misc/HHWM8.NRNI.dly.mmd.txt", skiprows=1)[15159:29037,3]
+    # compute RMSE from 1980-01-01 to 2000-12-31
+    Qsim = np.loadtxt(modelpath + "sample_model/output/wbvars.txt.HHWM8", skiprows=1)[3652:11322,18]
+    Qobs = np.loadtxt(modelpath + "sample_model/input/misc/HHWM8.NRNI.dly.mmd.txt", skiprows=1)[18811:26481,3]
+    RMSE = np.sqrt(((Qsim - Qobs) ** 2).mean())
+    return RMSE
+
+#######################################################
+# MAIN PROGRAM
+#======================================================
+def evaluate(x):
+    pf = util.read_param_file(controlFileName)
+    for n in range(pf['num_vars']):
+        pf['names'][n] = 'UQ_' + pf['names'][n]
+    genAppInputFile(x, appInputTmplts, appInputFiles, pf['num_vars'], pf['names'])
+    runApplication()
+    output = getOutput()
+    return output
---
+++ summa/summa_ASMO.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function, absolute_import
+import os
+import sys
+sys.path.append('../src')
+import ASMO
+import numpy as np
+import util
+import cPickle
+
+# model name
+modelname = 'SUMMA'
+model = __import__(modelname)
+
+# result path
+respath = '../../UQ-res/%s' % modelname
+if not os.path.exists(respath):
+    os.makedirs(respath)
+
+# load parameter name and range
+pf = util.read_param_file('%s.txt' % modelname)
+bd = np.array(pf['bounds'])
+nInput = pf['num_vars']
+xlb = bd[:,0]
+xub = bd[:,1]
+
+# run ASMO
+niter = 100
+bestx, bestf, x, y = ASMO.optimization(model, nInput, xlb, xub, niter)
+
+print('Optimum found by ASMO:')
+print('bestx:')
+print(bestx)
+print('bestf:')
+print(bestf)
+
+# save results to bin file
+with open('%s/SUMMA_ASMO.bin' % respath, 'w') as f:
+    cPickle.dump({'bestx': bestx, 'bestf': bestf, 'x': x, 'y': y}, f)
