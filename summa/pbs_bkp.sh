1bdea9cb2490781f8743c5b2df72d242d390dfb8
msaharia <manab@ucar.edu>
2018-03-21 10:59:42 -0600

    PBS scripts for Cheyenne

--- summa/pbs_bkp.sh
+++ summa/pbs_bkp.sh
@@ -3,11 +3,11 @@
 #PBS -N ASMO
 #PBS -m ea
 #PBS -q regular
-#PBS -l walltime=00:05:00
-#PBS -l select=1:ncpus=36:mpiprocs=1
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=36:mpiprocs=36
 #PBS -o ./log/asmo.out
 #PBS -e ./log/asmo.err
 mkdir -p /glade/scratch/manab/temp
 export TMPDIR=/glade/scratch/manab/temp
 export MPI_SHEPHERD=true
-/glade/u/home/manab/anaconda3/envs/py27/bin/python summa_ASMO.py > asmolog.txt
+mpiexec_mpt launch_cf.sh /glade/p/work/manab/fcast/ASMO/summa/asmo_joblist
